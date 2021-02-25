34da41d3ea75e0eae9bdaa06fb68c0a168712f12
studyhoroom <dlaghtjd4487@naver.com>
2021-02-25 23:58:20 +0900

    멀티 빌드 공부 (미완)

--- ve/vue.config.js
+++ ve/vue.config.js
@@ -2,4 +2,35 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+  pluginOptions : {
+    electronBuilder : {
+      builderOptions : {
+        appId : 'com.coworker.test',
+        nsis : {
+          shortcutName : 'coworker Tool Win'
+        },
+        dmg : {
+          title : "cowerker Tool Mac"
+        },
+        win : {
+          target : [
+            {
+              target : "nsis",
+              arch : [
+                'x64'
+              ]
+            }
+          ]
+        },
+        mac : {
+          category : "public.app-category.developer-tools",
+          target : [
+            {
+              target : "dmg"
+            }
+          ]
+        }
+      }
+    }
+  }
 };
